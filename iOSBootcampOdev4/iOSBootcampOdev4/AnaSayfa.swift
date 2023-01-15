b4f3ce1aa960428b96518daa93ed997dd0bb3602
Davut <davutkarakus3@gmail.com>
2023-01-15 22:26:24 +0300

    iOSBootcampOdev4

---
+++ iOSBootcampOdev4/iOSBootcampOdev4/AnaSayfa.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBootcampOdev4
+//
+//  Created by Davut Karakuş on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
