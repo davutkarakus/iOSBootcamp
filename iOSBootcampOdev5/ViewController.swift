98faf9d6847dfaecf81ef0baff07f6752e62175f
Davut <davutkarakus3@gmail.com>
2023-01-18 16:08:54 +0300

    Initial Commit

---
+++ iOSBootcampOdev5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBootcampOdev5
+//
+//  Created by Davut Karakuş on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
